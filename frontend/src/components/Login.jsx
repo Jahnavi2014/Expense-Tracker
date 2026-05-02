45a3f66ff553d14d8747072ff377ce022003fd96
Jahnavi Chowdhary <jahnavijanu836@gmail.com>
2026-05-02 14:25:16 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,7 +8,7 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
-const Login = ({onLogin, API_URL="https://expense-tracker-7jlx.onrender.com/api"}) => {
+  const Login = ({ onLogin, API_URL = import.meta.env.VITE_API_URL }) => {
   const [email,setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword,setShowPassword] =useState(false);
