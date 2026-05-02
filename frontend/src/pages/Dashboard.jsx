3ee7d223455331479fb7730931fdaf403ae62ef7
Jahnavi Chowdhary <jahnavijanu836@gmail.com>
2026-05-02 12:54:50 +0530

    Update API_BASE URL for production environment

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -13,7 +13,7 @@ import GaugeCard from '../components/GaugeCard';
 import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
 import AddTransactionModal from '../components/Add';
 
-const API_BASE ="http://localhost:4000/api";
+const API_BASE = "https://expense-tracker-7jlx.onrender.com/api";
 const getAuthHeader =()=>{
   const token =localStorage.getItem("token") || localStorage.getItem("authToken")
   return token ? {Authorization : `Bearer ${token}`} : {};
